76e0c1a85a51a0b2aeb950a07f5438b8893a36fe
Graf Stor <bratakmckey@gmail.com>
2020-02-19 19:59:53 +0300

    version 5.4
    tabs to spaces

--- Graf_Stor_Arch/audio.py
+++ Graf_Stor_Arch/audio.py
@@ -6,85 +6,85 @@ import eyed3
 
 class Audio:
 
-	def __init__(self):
-		self.track_paths = []
-		self.ispause = True
-		self.main_length = 1
-		self.nowplay = -1
-		self.volume = 70
+    def __init__(self):
+        self.track_paths = []
+        self.ispause = True
+        self.main_length = 1
+        self.nowplay = -1
+        self.volume = 70
 
-		mixer.init(44100, -16, 64, 8192)
-		self.set_volume(0)
+        mixer.init(44100, -16, 64, 8192)
+        self.set_volume(0)
 
-	def play(self, num):
-		mixer.music.load(self.track_paths[num])
-		mixer.music.play()
+    def play(self, num):
+        mixer.music.load(self.track_paths[num])
+        mixer.music.play()
 
-		self.nowplay = num
-		self.main_length = self.get_track_length(num)
-		self.ispause = False
+        self.nowplay = num
+        self.main_length = self.get_track_length(num)
+        self.ispause = False
 
-	def pause(self):
-		mixer.music.pause()
+    def pause(self):
+        mixer.music.pause()
 
-		self.ispause = True
+        self.ispause = True
 
-	def unpause(self):
-		mixer.music.unpause()
-		
-		self.ispause = False
+    def unpause(self):
+        mixer.music.unpause()
+        
+        self.ispause = False
 
-	def playlist_bild(self, path):
-		names_playlist = []
-		music_names = listdir(path)
-		music_names = [i for i in music_names if i.find("mp3") != -1]
+    def playlist_bild(self, path):
+        names_playlist = []
+        music_names = listdir(path)
+        music_names = [i for i in music_names if i.find("mp3") != -1]
 
-		for track_name in music_names:
-			track_path = f'{path}\\{track_name}'
-			track_info = eyed3.load(track_path)
-			track_author = ' '
+        for track_name in music_names:
+            track_path = f'{path}\\{track_name}'
+            track_info = eyed3.load(track_path)
+            track_author = ' '
 
-			try:
-				artist = track_info.tag.artist.encode("cp1252").decode("cp1251")
-				n_artist = artist.split()
-				for word in n_artist:
-					if word.lower() in track_name.lower():
-						break
-				else:
-					track_author = f' - {artist}'
+            try:
+                artist = track_info.tag.artist.encode("cp1252").decode("cp1251")
+                n_artist = artist.split()
+                for word in n_artist:
+                    if word.lower() in track_name.lower():
+                        break
+                else:
+                    track_author = f' - {artist}'
 
-			except:
-				pass
+            except:
+                pass
 
-			self.track_paths.append(track_path)
-			names_playlist.append(f'     {track_name[:-4]}{track_author}')
-		return names_playlist
+            self.track_paths.append(track_path)
+            names_playlist.append(f'     {track_name[:-4]}{track_author}')
+        return names_playlist
 
-	def set_timeline(self, time): 
-		mixer.music.play(0, time)
+    def set_timeline(self, time): 
+        mixer.music.play(0, time)
 
-	def set_volume(self,num):
-		self.volume += num
-		mixer.music.set_volume((100-(self.volume))/100)
+    def set_volume(self,num):
+        self.volume += num
+        mixer.music.set_volume((100-(self.volume))/100)
 
-	def get_now_play(self):
-		return self.nowplay
+    def get_now_play(self):
+        return self.nowplay
 
-	def get_track_length(self, num):
-		music =  MP3(self.track_paths[num])
-		return int(music.info.length)
+    def get_track_length(self, num):
+        music =  MP3(self.track_paths[num])
+        return int(music.info.length)
 
-	def get_length(self):
-		return self.main_length
+    def get_length(self):
+        return self.main_length
 
-	def get_time(self):
-		return mixer.music.get_pos()//1000
+    def get_time(self):
+        return mixer.music.get_pos()//1000
 
-	def get_volume(self):
-		return self.volume
+    def get_volume(self):
+        return self.volume
 
-	def is_pause(self):
-		return self.ispause
+    def is_pause(self):
+        return self.ispause
 
-	def is_busy(self):
-		return mixer.music.get_busy()
+    def is_busy(self):
+        return mixer.music.get_busy()
--- Graf_Stor_Arch/main.py
+++ Graf_Stor_Arch/main.py
@@ -1,20 +1,19 @@
 #Graf_Player
 
 '''
-	author: grafstor
-	date: 22.01.2020
+    author: grafstor
+    date: 22.01.2020
 
-	version 5.4:
-		- refaktoring
+    version 5.4:
+        - refaktoring
 
-	version 5.3:
-		- file encode problems
- 		- volume
-		- tracks rewinding
-		- rewinding
-		- audio list
-		- pause
-
+    version 5.3:
+        - file encode problems
+        - volume
+        - tracks rewinding
+        - rewinding
+        - audio list
+        - pause
 '''
 
 __version__ = "5.4"
@@ -28,143 +27,143 @@ from display import Display
 from audio import Audio
 
 class Player:
+    
+    def __init__(self,root):
+        self.audioplayer = Audio()
+        self.ap = self.audioplayer
+        self.filemanager = File_Manager()
+        self.display = Display(root)
+        self.dp = self.display
 
-	def __init__(self,root):
-		self.audioplayer = Audio()
-		self.ap = self.audioplayer
-		self.filemanager = File_Manager()
-		self.display = Display(root)
-		self.dp = self.display
+        self.main_time_change = 0
 
-		self.main_time_change = 0
+        self.display.draw_mainwindow(self.togle_play,
+                                     self.togle_list,
+                                     self.on_mousewheel,
+                                     self.close,
+                                     self.togle_play,)
 
-		self.display.draw_mainwindow(self.togle_play,
-									 self.togle_list,
-									 self.on_mousewheel,
-									 self.close,
-									 self.togle_play,)
+        main_path = self.filemanager.get_path()
 
-		main_path = self.filemanager.get_path()
+        playlist = self.ap.playlist_bild(main_path)
+        self.playlist_length = len(playlist)
 
-		playlist = self.ap.playlist_bild(main_path)
-		self.playlist_length = len(playlist)
+        self.dp.draw_list(playlist)
 
-		self.dp.draw_list(playlist)
+        self.dp.look_root()
 
-		self.dp.look_root()
+        sleep(1.2)
 
-		sleep(1.2)
+        for name in sys.argv:
+            if name.isdigit():
+                self.play_track(randint(1,len(playlist)))
+            else:
+                for track in range(len(playlist)):
+                    if name in playlist[track].lower():
+                        self.play_track(track)
+                        if not randint(0,6):
+                            break
 
-		for name in sys.argv:
-			if name.isdigit():
-				self.play_track(randint(1,len(playlist)))
-			else:
-				for track in range(len(playlist)):
-					if name in playlist[track].lower():
-						self.play_track(track)
-						if not randint(0,6):
-							break
+        self.main_frame()
 
-		self.main_frame()
+    def main_frame(self):
+        if self.dp.is_winhover():
+            if not self.dp.is_hover(40,100,260):
+                if not (self.dp.is_hover(440,100,260) and self.dp.is_listopen()):
+                    if self.dp.is_listopen():
+                        self.dp.hide_list(self.ap.get_now_play())
+                    self.dp.hide_root()
 
-	def main_frame(self):
-		if self.dp.is_winhover():
-			if not self.dp.is_hover(40,100,260):
-				if not (self.dp.is_hover(440,100,260) and self.dp.is_listopen()):
-					if self.dp.is_listopen():
-						self.dp.hide_list(self.ap.get_now_play())
-					self.dp.hide_root()
+        else:
+            if self.dp.is_hover(40,100,260):
+                self.dp.look_root()
 
-		else:
-			if self.dp.is_hover(40,100,260):
-				self.dp.look_root()
+        if not self.ap.is_busy() and not self.ap.is_pause():
+            self.play_next()
+        now_time = self.ap.get_time() + self.main_time_change
+        self.dp.set_timeline_long(round(360*now_time/self.ap.get_length()))
 
-		if not self.ap.is_busy() and not self.ap.is_pause():
-			self.play_next()
-		now_time = self.ap.get_time() + self.main_time_change
-		self.dp.set_timeline_long(round(360*now_time/self.ap.get_length()))
+        self.dp.root.after(160,self.main_frame)
 
-		self.dp.root.after(160,self.main_frame)
+    def play_track(self,num):
+        self.dp.button_play()
+        self.dp.set_timeline_long(0)
 
-	def play_track(self,num):
-		self.dp.button_play()
-		self.dp.set_timeline_long(0)
+        self.ap.play(num)
 
-		self.ap.play(num)
+        self.main_time_change = 0
 
-		self.main_time_change = 0
+    def play_next(self):
+        now_track = self.ap.get_now_play()
+        if now_track + 1 > self.playlist_length -1:
+            now_track = -1
+        self.dp.select_track(now_track + 1)
+        self.play_track(now_track + 1)
 
-	def play_next(self):
-		now_track = self.ap.get_now_play()
-		if now_track + 1 > self.playlist_length -1:
-			now_track = -1
-		self.dp.select_track(now_track + 1)
-		self.play_track(now_track + 1)
+    def play_last(self):
+        now_track = self.ap.get_now_play()
+        if now_track - 1 < 0:
+            now_track = self.playlist_length
+        self.dp.select_track(now_track - 1)
+        self.play_track(now_track - 1)
 
-	def play_last(self):
-		now_track = self.ap.get_now_play()
-		if now_track - 1 < 0:
-			now_track = self.playlist_length
-		self.dp.select_track(now_track - 1)
-		self.play_track(now_track - 1)
+    def pause_track(self):
+        self.dp.button_pause()
 
-	def pause_track(self):
-		self.dp.button_pause()
+        self.ap.pause()
 
-		self.ap.pause()
+    def unpause_track(self):
+        self.dp.button_play()
 
-	def unpause_track(self):
-		self.dp.button_play()
+        self.ap.unpause()
 
-		self.ap.unpause()
+    def togle_list(self,event):
+        if self.dp.is_listopen():
+            self.dp.hide_list(self.ap.get_now_play())
+        else:
+            self.dp.look_list(self.ap.get_now_play())
 
-	def togle_list(self,event):
-		if self.dp.is_listopen():
-			self.dp.hide_list(self.ap.get_now_play())
-		else:
-			self.dp.look_list(self.ap.get_now_play())
+    def on_mousewheel(self,event):
+        if self.dp.is_winhover():
+            dire = -1*(event.delta/120)*5
+            volume = self.ap.get_volume()
+            if self.dp.is_hover(40,130,230):
+                if not (volume+dire>100.0 or volume+dire<15):
+                    self.ap.set_volume(dire)
+                    self.dp.move_volume(dire)
 
-	def on_mousewheel(self,event):
-		if self.dp.is_winhover():
-			dire = -1*(event.delta/120)*5
-			volume = self.ap.get_volume()
-			if self.dp.is_hover(40,130,230):
-				if not (volume+dire>100.0 or volume+dire<15):
-					self.ap.set_volume(dire)
-					self.dp.move_volume(dire)
+            elif self.dp.is_hover(40,230,260):
+                if dire < 0:
+                    self.play_next()
 
-			elif self.dp.is_hover(40,230,260):
-				if dire < 0:
-					self.play_next()
+                else:
+                    self.play_last()
 
-				else:
-					self.play_last()
+            elif self.dp.is_hover(40,100,130):
+                if self.main_time_change - dire < 360 and self.main_time_change - dire > 0:
+                    self.main_time_change = self.ap.get_time() + self.main_time_change
+                    self.main_time_change -= dire*1.7
+                    if not self.ap.is_pause():
+                        self.ap.set_timeline(self.main_time_change)
 
-			elif self.dp.is_hover(40,100,130):
-				if self.main_time_change - dire < 360 and self.main_time_change - dire > 0:
-					self.main_time_change = self.ap.get_time() + self.main_time_change
-					self.main_time_change -= dire*1.7
-					if not self.ap.is_pause():
-						self.ap.set_timeline(self.main_time_change)
+    def togle_play(self,event=0):
+        num = self.dp.get_now_select()
+        is_same = num == self.ap.get_now_play()
 
-	def togle_play(self,event=0):
-		num = self.dp.get_now_select()
-		is_same = num == self.ap.get_now_play()
-		
-		if not self.ap.is_pause() and is_same:
-			self.pause_track()
-		else:
-			if is_same:
-				self.unpause_track()
-			else:
-				if num != None:
-					self.play_track(num)
+        if not self.ap.is_pause() and is_same:
+            self.pause_track()
+        else:
+            if is_same:
+                self.unpause_track()
+            else:
+                if num != None:
+                    self.play_track(num)
 
-	def close(self,event):
-		self.dp.close()
-		exit()
+    def close(self,event):
+        self.dp.close()
+        exit()
 
 if __name__ == "__main__":
-	root = Tk()
-	main_player = Player(root)
-	mainloop()
+    root = Tk()
+    main_player = Player(root)
+    mainloop()
